9179e0c603d9fb5b1f2850ef9fa32652164d285d
KrzysztofJaronczyk <krzysztofjaronczykzyt@gmail.com>
2024-05-09 16:56:42 +0200

    Add datepicker, styling

--- src/components/screens/Index.tsx
+++ src/components/screens/Index.tsx
@@ -1,37 +1,33 @@
-import React from 'react';
-import { Dialog } from '@headlessui/react';
+import React, { useEffect, useState } from 'react';
 import { collection, getDocs, query, addDoc } from 'firebase/firestore';
-import { useEffect, useRef, useState } from 'react';
-import { useAuthState } from '~/components/contexts/UserContext';
-import { SignInButton } from '~/components/domain/auth/SignInButton';
-import { SignOutButton } from '~/components/domain/auth/SignOutButton';
-import { Head } from '~/components/shared/Head';
-import { useFirestore, useStorage } from '~/lib/firebase';
+import { useFirestore } from '~/lib/firebase';
 import { Bounce, ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export type Project = {
   id: string;
   title: string;
   desc: string;
+  created: Date | null; // Change created type to Date | null
 };
 
 enum InputEnum {
   Id = 'id',
   Title = 'title',
   Description = 'desc',
+  Created = 'created',
 }
 
 function Index() {
-  // const { state } = useAuthState();
-  const [projects, setprojects] = useState<Array<Project>>([]);
+  const [projects, setProjects] = useState<Array<Project>>([]);
   const firestore = useFirestore();
-  const storage = useStorage();
   const [inputData, setInputData] = useState<Partial<Project>>({
     title: '',
     desc: '',
+    created: new Date(),
   });
-  const [image, setImage] = useState('');
   const [formError, setFormError] = useState<boolean>(false);
 
   useEffect(() => {
@@ -41,57 +37,57 @@ function Index() {
       const querySnapshot = await getDocs(projectsQuery);
       const fetchedData: Array<Project> = [];
       querySnapshot.forEach((doc) => {
-        fetchedData.push({ id: doc.id, ...doc.data() } as Project);
+        const data = doc.data();
+        if (data.created) {
+          // Check if created field exists
+          const createdDate = data.created.toDate(); // Parse Firestore Timestamp to Date object
+          fetchedData.push({ id: doc.id, ...data, created: createdDate } as Project);
+        } else {
+          fetchedData.push({ id: doc.id, ...data, created: null } as Project); // If created field is missing, set to null
+        }
       });
-      // console.log(fetchedData);
-
-      setprojects(fetchedData);
+      setProjects(fetchedData);
     }
     fetchData();
   }, []);
 
-  const handleInputChange = (field: InputEnum, value: string) => {
+  const handleInputChange = (field: InputEnum, value: string | Date | null) => {
     setInputData({
       ...inputData,
-      [field]: value,
+      [field]: value || new Date(), // If null, set to current date
     });
   };
 
   const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    //don't reload the page
     e.preventDefault();
-    //>>project crud operations<<
-    //ADD new project
-    try{
-     const projectsCollection = collection(firestore, 'project');
+    try {
+      const projectsCollection = collection(firestore, 'project');
       const newProject: Partial<Project> = {
         title: inputData.title,
         desc: inputData.desc,
+        created: inputData.created as Date,
       };
-      // console.log(newProject);
       await addDoc(projectsCollection, newProject);
       toast.success('🦄 Project has been added!', {
-        position: "top-right",
+        position: 'top-right',
         autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: "dark",
+        theme: 'dark',
         transition: Bounce,
-        });
-      //update the state
-      setprojects([...projects, newProject as Project]);
-      setInputData({ title: '', desc: '' });
+      });
+      setProjects([...projects, newProject as Project]);
+      setInputData({ title: '', desc: '', created: new Date() });
     } catch (error) {
-     setFormError(true);
+      setFormError(true);
     }
   };
 
   return (
     <>
-      <Head title="TOP PAGE" />
       <div className="hero min-h-screen bg-slate-800">
         <div className="max-w-5xl mx-auto">
           <form className="flex items-center" onSubmit={handleFormSubmit}>
@@ -106,7 +102,13 @@ function Index() {
               type="text"
               onChange={(e) => handleInputChange(InputEnum.Description, e.target.value)}
               value={inputData.desc}
-              placeholder='description'
+              placeholder="description"
+              className="m-4 text-slate-50 bg-transparent border border-slate-700 focus:ring-slate-400 focus:outline-none p-4 rounded-lg"
+            />
+            <DatePicker
+              selected={inputData.created as Date}
+              onChange={(date) => handleInputChange(InputEnum.Created, date)}
+              dateFormat="dd/MM/yyyy" // Set date format to DD/MM/YYYY
               className="m-4 text-slate-50 bg-transparent border border-slate-700 focus:ring-slate-400 focus:outline-none p-4 rounded-lg"
             />
             <button
@@ -116,22 +118,22 @@ function Index() {
               Add new project
             </button>
           </form>
-          <table className="table w-full text-slate-50">
-            <thead>
-              <tr>
-                <th className="bg-slate-900 border border-slate-700 text-slate-50 uppercase">Title</th>
-                <th className="bg-slate-900 border border-slate-700 text-slate-50 uppercase">Description</th>
-              </tr>
-            </thead>
-            <tbody>
-              {projects.map((project) => (
-                <tr key={project.id} className="bg-transparent">
-                  <td className="border border-slate-700">{project.title}</td>
-                  <td className="border border-slate-700">{project.desc}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <div className="grid grid-cols-3 gap-4 w-full text-slate-50">
+            {projects.map((project) => (
+              <div
+                key={project.id}
+                className="h-40 rounded-md flex flex-col justify-between shadow-slate-900 shadow-md p-4 bg-gradient-to-r from-slate-800 to-slate-700"
+              >
+                <div>
+                  <div className="text-xl mb-2 font-bold">Project: {project.title}</div>
+                  <div className="font-xl">{project.desc}</div>
+                </div>
+                <div className="text-slate-200 text-right text-md text-opacity-60 font-semibold">                  
+                  {project.created ? project.created.toDateString() : 'No date'}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <ToastContainer />
